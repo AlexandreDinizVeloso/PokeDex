dd4a9b4ee1ab6540bb6424c7020ffb8db35d3069
AlexandreDinizVeloso <alexandredinizveloso@gmail.com>
2024-02-10 18:04:47 -0300

    Redimensionamento dos cards

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect, SetStateAction } from 'react';
+import Pagination from '../../components/pagination';
+
 import forestBackground from '../../assets/background-types/forest.png';
 import cavernBackground from '../../assets/background-types/cavern.png';
 import yforestBackground from '../../assets/background-types/yforest.png';
@@ -126,7 +128,6 @@ function PokemonList() {
         return '';
     }
   };
-  
 
   return (
     <div className="list-page">
@@ -151,36 +152,13 @@ function PokemonList() {
           ))}
         </div>
       </main>
-      <div className="pagination-buttons">
-      {previousUrl && <button className="pagination-button" id='pagination-previous' onClick={() => handleLoadMore(previousUrl, -1)}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-        </svg>
-        </button>}
-        {Array.from({ length: 5 }).map((_, i) => {
-          const page = currentPage - 2 + i;
-          return page > 0 && (
-            <button
-              key={page}
-              className="pagination-button"
-              id='pagination-number'
-              onClick={() => handleLoadPage(page)}
-              style={{ 
-                fontWeight: currentPage === page ? 'bold' : 'normal',
-                color: currentPage === page ? '#3A463E' : '#F9F7F9',
-                backgroundColor: currentPage === page ? '#98D2CE' : '#698880',
-              }}
-            >
-              {page}
-            </button>
-          );
-        })}
-        {nextUrl && <button className="pagination-button" id='pagination-next' onClick={() => handleLoadMore(nextUrl, 1)}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-        </svg>
-        </button>}
-      </div>
+      <Pagination 
+        handleLoadMore={handleLoadMore} 
+        handleLoadPage={handleLoadPage} 
+        currentPage={currentPage}
+        previousUrl={previousUrl}
+        nextUrl={nextUrl}
+      />
     </div>
   );
 }
